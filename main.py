65a449e90751d4858ae9b1c7bac12290061e4042
Bendan04 <bendan04@outlook.com>
2025-11-21 22:05:32 +0000

    Added database connection.
    Added render template for list.html, where it lists all items in cs2 weapons database.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3 as db
 
 app = Flask(__name__)
+conn = db.connect('cs2_weapons.db')
 
 @app.route('/')
 def index():
@@ -23,6 +25,19 @@ def create_trade_offer():
 @app.route('/chat')
 def chat():
     return render_template('chat.html')
+
+@app.route('/list')
+def list_items():
+    conn = db.connect('cs2_weapons.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name, rarity, stattrak, souvenir, image FROM items")
+    items = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('list.html', items=items)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
